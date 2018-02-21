3f2d4e56bf8b7b471426627e389444d6a1273210
JimvandeVen <Jim.van.de.Ven@hva.nl>
2018-02-21 16:04:14 +0100

    Added the pages i need for the first test. Some need to be filled still.

---
+++ js/java.js
@@ -0,0 +1,21 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/* LOGIN */
+
+var main = document.querySelector(".body");
+var inlogPlek = document.querySelector(".inloggen");
+
+function openInlog() {
+    inlogPlek.classList.remove("hidden");
+}
+
+function closeInlog() {
+    inlogPlek.classList.add("hidden");
+}
+
+main.addEventListener("click", openInlog);
+
+
+
+//inlogKnop.addEventListener("click", openInlog);
+//var inlogKnop = document.querySelector(".main");
